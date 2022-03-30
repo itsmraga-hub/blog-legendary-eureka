86bb71d380466618366b00029ab6461e8443a235
Raga <ragawilliam570@gmail.com>
2022-03-30 20:19:05 +0300

    Removed secret key

--- BLOG/settings.py
+++ BLOG/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5+qca$z6&4aa(o7_nts=hu@4c*x_zirdtwsoo4leaoqbyhbt=q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
